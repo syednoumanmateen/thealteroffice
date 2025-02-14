52bbf4602f61c85e898cf3e627507d88b3baf905
syed <syednoumanmateen1997@gmail.com>
2025-02-14 15:56:41 +0530

    images

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import notFoundImg from "../assets/notfound.png";
 import CenterScreen from './CenterScreen';
 
 const NotFound: React.FC = () => {
     return (
         <CenterScreen>
             <section className="text-center">
-                <img src={notFoundImg} alt="Page not found" className="mb-3" />
+                <img src="https://res.cloudinary.com/dz4lki63f/image/upload/v1739528732/notfound_on3era.png" alt="Page not found" className="mb-3" />
                 <h2 className="text-xl font-semibold">Oops! Page Not Found</h2>
                 <p className="text-gray-600 mb-4">The page you are looking for does not exist.</p>
                 <Link to="/">
